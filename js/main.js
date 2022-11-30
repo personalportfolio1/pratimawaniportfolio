66f1d201e546e2ac80d46d8adeda034eea9fcd55
Pratima Wani <pratimawani271998@gmail.com>
2022-11-30 17:41:14 +0530

    portfolio edited

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
        $('.navbar-collapse').collapse('hide');
    });
 
-   let nav=document.querySelector(".navbarsection");
-   window.onscroll=function(){
-       if(document.documentElement.scrollTop > 20){
-           nav.classList.add("scroll-on");
-       }
-           else{
-               nav.classList.remove("scroll-on");
-           }
-       }
+//    let nav=document.querySelector(".navbar");
+//    window.onscroll=function(){
+//        if(document.documentElement.scrollTop > 20){
+//            nav.classList.add("navbarsection");
+//        }
+//            else{
+//                nav.classList.remove("scroll-on");
+//            }
+//        }
 });
