bc3f43dc7542ddea421dd185fe8baf845b25d251
Pratima Wani <pratimawani271998@gmail.com>
2022-12-03 16:12:43 +0530

    scrool-on class added on navbar to gixed the navbar top

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ navCollapse.classList.remove("show");
 });
 });
 
-//    let nav=document.querySelector(".navbar");
-//    window.onscroll=function(){
-//        if(document.documentElement.scrollTop > 20){
-//            nav.classList.add("navbarsection");
-//        }
-//            else{
-//                nav.classList.remove("scroll-on");
-//            }
-//        }
+   let nav=document.querySelector(".navbar");
+   window.onscroll=function(){
+       if(document.documentElement.scrollTop > 20){
+           nav.classList.add("scroll-on");
+       }
+           else{
+               nav.classList.remove("scroll-on");
+           }
+       }
 });
